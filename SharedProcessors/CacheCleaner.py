e0001da85e5c7eab5a3ff31d4fa88e9e9d12132b
James Smith <james@smithjw.me>
2024-02-14 13:27:58 +1100

    Updating CacheCleaner

--- SharedProcessors/CacheCleaner.py
+++ SharedProcessors/CacheCleaner.py
@@ -18,19 +18,19 @@
 from fnmatch import fnmatch
 from pathlib import Path
 
-from autopkglib import Processor
+from autopkglib import Processor, ProcessorError
 
 __all__ = ["CacheCleaner"]
 
 
 def get_size(folder: str) -> int:
-    size_in_bytes = sum(p.stat().st_size for p in Path(folder).rglob("*"))
-    for unit in ("B", "K", "M", "G", "T"):
-        if size_in_bytes < 1024:
-            break
-        size_in_bytes /= 1024
+    dir_size = sum(p.stat().st_size for p in Path(folder).rglob("*"))
 
-    readable_size = f"{size_in_bytes:.1f}{unit}"
+    for unit in ("B", "K", "M", "G", "T"):  # noqa: B007
+        if dir_size < 1024:
+            break
+        dir_size /= 1024
+    readable_size = f"{dir_size:.1f}{unit}"
 
     return readable_size
 
@@ -43,8 +43,13 @@ class CacheCleaner(Processor):
     input_variables = {
         "RECIPE_CACHE_DIR": {"required": False, "description": ("RECIPE_CACHE_DIR.")},
         "file_retention_patterns": {
-            "description": ("Pattern(s) to identify all files we're retaining"),
+            "description": "Pattern(s) to identify all files we're retaining",
             "default": ["*.plist", "*.info.json"],
+            "required": False,
+        },
+        "delete_files_folders": {
+            "description": "Determines if the Processor will attempt to remove items within the cache folder not matching the patterns",
+            "default": True,
             "required": False,
         },
     }
@@ -64,44 +69,58 @@ class CacheCleaner(Processor):
     description = __doc__
 
     def main(self):
-        retention_patterns = self.env["file_retention_patterns"]
-        if isinstance(retention_patterns, str):
-            retention_patterns = [retention_patterns]
+        try:
+            self.recipe_cache_dir = Path(self.env.get("RECIPE_CACHE_DIR", None))
+        except TypeError:
+            raise ProcessorError("RECIPE_CACHE_DIR not found") from None
+
+        self.file_retention_patterns = (
+            self.env.get("file_retention_patterns")
+            if isinstance(self.env.get("file_retention_patterns"), list)
+            else [self.env.get("file_retention_patterns")]
+        )
 
         recipe_cache_dir = Path(self.env["RECIPE_CACHE_DIR"])
         self.output(f"Looking for files under {recipe_cache_dir}")
 
-        recipe_cache_dir_size = get_size(recipe_cache_dir)
-        self.env["folder_size_pre"] = recipe_cache_dir_size
-        self.output(f"Folder size before CacheCleaner: {recipe_cache_dir_size}")
+        self.recipe_cache_dir_size = get_size(recipe_cache_dir)
+        self.env["folder_size_pre"] = self.recipe_cache_dir_size
+        self.output(f"Folder size before CacheCleaner: {self.recipe_cache_dir_size}")
 
         files_to_remove = [
             item
             for item in recipe_cache_dir.rglob("*")
-            if not any(fnmatch(item, pattern) for pattern in retention_patterns)
+            if not any(
+                fnmatch(item, pattern) for pattern in self.file_retention_patterns
+            )
             and (not item.is_dir() or len(list(item.iterdir())) == 0)
         ]
 
         self.env["removed_files"] = []
         removed_files = self.env["removed_files"]
 
-        for item in files_to_remove:
-            if item.is_dir():
-                self.output(f"Removing directory: {item}")
-                item.rmdir()
+        # for item in files_to_remove:
+        #     if not self.env["delete_files_folders"]:
+        #         removed_files.append(str(item))
+        #         continue
 
-            if item.is_file(missing_ok=True):
-                self.output(f"Removing file: {item}")
-                item.unlink()
+        #     if item.is_dir():
+        #         self.output(f"Removing directory: {item}")
+        #         item.rmdir()
 
-            if item.exists():
-                self.output(f"Could not remove {item}")
-            else:
-                removed_files.append(str(item))
+        #     if item.is_file():
+        #         self.output(f"Removing file: {item}")
+        #         item.unlink(missing_ok=True)
 
-        recipe_cache_dir_size = get_size(recipe_cache_dir)
-        self.env["folder_size_post"] = recipe_cache_dir_size
-        self.output(f"Folder size after CacheCleaner: {recipe_cache_dir_size}")
+        #     if item.exists():
+        #         self.output(f"Could not remove {item}")
+
+        #     else:
+        #         removed_files.append(str(item))
+
+        self.recipe_cache_dir_size = get_size(recipe_cache_dir)
+        self.env["folder_size_post"] = self.recipe_cache_dir_size
+        self.output(f"Folder size after CacheCleaner: {self.recipe_cache_dir_size}")
         self.output(f"Items removed: {removed_files}")
 
 
